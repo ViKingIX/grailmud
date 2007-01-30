7b18270b9e6eac1fc5cf9ffda35554af2edb5fd9
catrin <catrin@9d540161-2e2a-0410-b27f-9f768c06348c>
2007-01-30 22:43:35 +0000

    Document my shiny new module

--- trunk/morelimiter.py
+++ trunk/morelimiter.py
@@ -2,25 +2,37 @@ import re
 
 chunk_pattern = r"(.*\n){%d}|(.*\n){%d}.*"
 
-def indnum(s, t, n):
+def indnum(string, target, num):
+    """Find the index of the num-th occurence of target in string.
+
+    Raises ValueErrors if there aren't enough targets."""
     ind = 0
-    for _ in range(n):
-        #add one so we skip the one we just found
-        ind = s.index(t, ind) + 1
+    for _ in range(num):
+        #add so we skip the one we just found
+        ind = string.index(target, ind) + len(target)
     return ind
 
 class MoreLimiter(object):
+    """A recorder of what the limit is, and a producer of Chunkers that actually
+    limit the data.
+    """
 
     def __init__(self, limit):
         self.limit = limit
 
     def chunk(self, data):
+        """Return a Chunker that limits the data."""
         return Chunker(self, data)
 
     def change_limit(self, limit):
+        """Change the limit.
+
+        Tihs affects already-created Chunkers.
+        """
         self.limit = limit
 
 class Chunker(object):
+    """The object that actually does the chunking."""
 
     def __init__(self, limiter, data):
         self.limiter = limiter
@@ -30,6 +42,7 @@ class Chunker(object):
         self.initial_lines = self.lines_left = data.count('\n') + 1
 
     def next(self):
+        """Return the next chunk, a string with a set number of lines."""
         if not self.lines_left:
             raise StopIteration()
         try:
@@ -42,3 +55,6 @@ class Chunker(object):
             self.data = self.data[nl_ind:]
             self.lines_left -= self.limiter.limit
         return res
+
+    def __iter__(self):
+        return self
