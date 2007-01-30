14fcf66b961c1ce4a6848765553641f3576841da
catrin <catrin@9d540161-2e2a-0410-b27f-9f768c06348c>
2007-01-30 20:52:19 +0000

    more tests

---
+++ trunk/actiondefs/test/test_system.py
@@ -0,0 +1,29 @@
+from grail2.actiondefs.system import UnfoundObjectEvent, PermissionDeniedEvent,\
+                                     BadSyntaxEvent, BlankLineEvent, blankLine,\
+                                     permissionDenied, badSyntax, \
+                                     unfoundObject, register
+from grail2.tests.helper import SetupHelper
+
+def test_registration():
+    d = {}
+    register(d)
+    assert d[''] is blankLine
+
+class TestEvents(SetupHelper):
+
+    def test_bad_syntax_without_argument(self):
+        badSyntax(self.obj)
+        assert self.obj.listener.received == [BadSyntaxEvent(None)]
+
+    def test_bad_syntax_with_argument(self):
+        arg = "foo"
+        badSyntax(self.obj, arg)
+        assert self.obj.listener.received == [BadSyntaxEvent(arg)]
+
+    def test_blank_line(self):
+        blankLine(self.obj, "foo", None)
+        assert self.obj.listener.received == [BlankLineEvent()]
+
+    def test_permission_denied(self):
+        permissionDenied(self.obj)
+        assert self.obj.listener.received == [PermissionDeniedEvent()]
