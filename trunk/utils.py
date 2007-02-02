72ba045cf8b0d523c965dd5587bb42b211f1c8e6
catrin <catrin@9d540161-2e2a-0410-b27f-9f768c06348c>
2007-02-02 02:22:09 +0000

    remove debugging statements, too

--- trunk/utils.py
+++ trunk/utils.py
@@ -70,24 +70,17 @@ class InstanceVariableFactoryObject(object):
     __metaclass__ = InstanceVariableFactoryMetaclass
     
     def __getattribute__(self, attr):
-        print '__getattribute__ called'
         if attr not in ('__dict__', '__class__'):
-            print 'Not a special name'
             if attr not in self.__dict__:
-                print 'Not in self dict'
                 for cls in type(self).__mro__:
                     if attr in getattr(cls, '_instance_variable_factories',
                                        {}):
                         res = cls._instance_variable_factories[attr](self)
                         setattr(self, attr, res)
                         return res
-                print 'No default factories'
 
                 if not any((attr in cls.__dict__) for cls in type(self).__mro__):
-                    print 'Raising'
                     raise AttributeError()
-
-        print 'Special name? Failure? Who knows?'
             
         return object.__getattribute__(self, attr)
 
